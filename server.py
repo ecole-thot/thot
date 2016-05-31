725ae7c323d8707d62c1f0742a4f5b65c1dfdc9b
tchapi <regbasket@gmail.com>
2016-05-31 23:43:54 +0200

    New links

--- server.py
+++ server.py
@@ -30,27 +30,31 @@ NB_STUDENTS = 40
 PRESS = [
     { "link": "https://savoirs.rfi.fr/fr/communaute/langue-francaise/ma-parole-on-vous-presente-thot",
       "title": "RFI — Émission « Danse avec les mots »",
-      "text": "Chronique « Ma Parole » : on vous présente Thot"
+      "text": "Chronique « Ma Parole » : on vous présente Thot (11/04/2016)"
     },
     { "link": "http://www.liberation.fr/direct/element/une-ecole-pour-migrants-gratuite-et-diplomante-pourrait-voir-le-jour-en-france_35401/",
       "title": "Libération",
       "text": "Une école pour migrants gratuite et diplômante pourrait voir le jour en France (18/04/2016)"
     },
+    { "link": "http://www.rfi.fr/emission/20160418-institut-francais-bucarest",
+      "title": "RFI",
+      "text": "Chronique « Ma Parole » : Interview d'Imaad Ali, directeur pédagogique de l'école Thot - 17'55 (18/04/16)"
+    },
     { "link": "http://agi.to/agitox-actualite-fle-edu-76/",
       "title": "Agito", 
-      "text": "Thot FLE, une école diplômante pour migrants"
+      "text": "Thot FLE, une école diplômante pour migrants (22/04/2016)"
     },
     { "link": "http://consocollaborative.com/article/elles-lancent-une-ecole-de-francais-pour-les-refugies-grace-au-crowdfunding/",
       "title": "Conso Collaborative",
-      "text": "Elles lancent une école de français pour les réfugiés"
-    },
-    { "link": "http://www.lecourrier-du-soir.com/articles/h%C3%A9lo%C3%AFse-%C2%AB-le-de-l%E2%80%99association-thot-est-que-les-r%C3%A9fugi%C3%A9s-fassent-partie-de-la-france-%C2%BB",
-      "title": "Le Courrier du Soir", 
-      "text": "Le but de l’association Thot est que les réfugiés fassent partie de la France"
+      "text": "Elles lancent une école de français pour les réfugiés (26/04/2016)"
     },
     { "link": "https://rcf.fr/actualite/journal-regional-du-mardi-26-avril",
       "title": "RCF",
       "text": "Journal Régional — 1'36\" (26/04/2016)"
+    },
+    { "link": "http://www.lecourrier-du-soir.com/articles/h%C3%A9lo%C3%AFse-%C2%AB-le-de-l%E2%80%99association-thot-est-que-les-r%C3%A9fugi%C3%A9s-fassent-partie-de-la-france-%C2%BB",
+      "title": "Le Courrier du Soir", 
+      "text": "Le but de l’association Thot est que les réfugiés fassent partie de la France (27/04/2016)"
     },
     { "link": "http://cheekmagazine.fr/societe/thot-judith-aquien-ecole-francais-refugies/",
       "title": "Cheek Magazine",
@@ -72,21 +76,25 @@ PRESS = [
       "title": "Cosmopolitaine, France Inter",
       "text": "Chronique de Marie-Madeleine Rigopoulos sur Thot - 26'30\" (01/05/2016)"
     },
+    { "link": "http://issuu.com/lcf-magazine/docs/lcff_magazine_n__40-_issuu/43?e=6017579/35344828",
+      "title": "Langue et culture françaises & francophones",
+      "text": "Le magazine LCFF nous offre une page de publicité (numéro 40, mai 2016) (01/05/2016)"
+    },
     { "link": "http://annieallmusic.blogspot.fr/2016/05/thot-un-grand-pas-vers-lintegration.html",
       "title": "Annie All Music",
       "text": "Thot, un grand pas vers l'intégration (02/05/2016)"
     },
-    { "link": "http://2016.wetalk-community.org/modeles-feminins/",
-      "title": "W(e) Talk",
-      "text": "Judith Aquien, Présidente de Thot, sélectionnée parmi les 8 modèles féminins 2016, à la conférence \"Artisanes du commun, qui forment ensemble pour un autre demain\" (21/05/2016)"
-    },
-    { "link": "http://issuu.com/lcf-magazine/docs/lcff_magazine_n__40-_issuu/43?e=6017579/35344828",
-      "title": "Langue et culture françaises & francophones",
-      "text": "Le magazine LCFF nous offre une page de publicité (numéro 40, mai 2016)"
+    { "link": "http://www.liberation.fr/debats/2016/05/02/ils-ferment-les-frontieres-ouvrons-nos-ecoles_1450031",
+      "title": "Libération",
+      "text": "\"Ils ferment les frontières, ouvrons nos écoles\" - tribune du Résome dans Libération (02/05/2016)"
     },
     { "link": "http://www.france4.fr/emissions/l-autre-jt/videos/replay_-_lautre_jt_05-05-2016_1142885",
       "title": "L'Autre JT",
       "text": "Sarah Constantin met 20/20 à Thot - 20'24'' (05/05/2016)"
+    },
+    { "link": "http://www.telerama.fr/monde/le-resome-pour-que-l-ecole-accueille-les-refugies-dans-la-dignite,141947.php",
+      "title": "Télérama",
+      "text": "Le Resome, pour que l’école accueille les réfugiés dans la dignité (05/05/2016)"
     },
     { "link": "https://www.youtube.com/watch?v=KqqYWuFPRpE&feature=youtu.be",
       "title": "LCI",
@@ -104,17 +112,13 @@ PRESS = [
       "title": "Pressée",
       "text": "« Avec Thot, on transmet un horizon aux réfugiés. » (11/05/2016)"
     },
+    { "link": "http://www.redattoresociale.it/Notiziario/Articolo/507633/In-Francia-apre-Thot-scuola-di-francese-per-richiedenti-asilo-e-rifugiati",
+      "title": "Redattore Sociale",
+      "text": "In Francia apre “Thot”, scuola di francese per richiedenti asilo e rifugiati (11/05/2016)"
+    },
     { "link": "https://savoir.actualitte.com/article/scolarite/1798/du-crowdfunding-pour-creer-une-ecole-apprenant-le-francais-aux-refugies",
       "title": "Actualitté",
       "text": "Du crowdfunding pour créer une école apprenant le français aux réfugiés (13/05/2016)"
-    },
-    { "link": "http://www.liberation.fr/debats/2016/05/02/ils-ferment-les-frontieres-ouvrons-nos-ecoles_1450031",
-      "title": "Libération",
-      "text": "\"Ils ferment les frontières, ouvrons nos écoles\" - tribune du Résome dans Libération (02/05/2016)"
-    },
-    { "link": "http://www.rfi.fr/emission/20160418-institut-francais-bucarest",
-      "title": "RFI",
-      "text": "Chronique « Ma Parole » : Interview d'Imaad Ali, directeur pédagogique de l'école Thot - 17'55 (18/04/16)"
     },
     { "link": "http://www.lecourrierdelatlas.com/1138518052016Une-ecole-pour-apprendre-le-francais-aux-refugies.html",
       "title": "Le Courrier de l'Atlas",
@@ -140,17 +144,25 @@ PRESS = [
       "title": "L'Obs",
       "text": "A Paris, une \"école des réfugiés\" pour apprendre le français (20/05/2016)"
     },
-    { "link": "http://www.telerama.fr/monde/le-resome-pour-que-l-ecole-accueille-les-refugies-dans-la-dignite,141947.php",
-      "title": "Télérama",
-      "text": "Le Resome, pour que l’école accueille les réfugiés dans la dignité "
+    { "link": "http://2016.wetalk-community.org/modeles-feminins/",
+      "title": "W(e) Talk",
+      "text": "Judith Aquien, Présidente de Thot, sélectionnée parmi les 8 modèles féminins 2016, à la conférence \"Artisanes du commun, qui forment ensemble pour un autre demain\" (21/05/2016)"
     },
     { "link": "http://lapartducolibri.fr/?p=837",
       "title": "La Part du colibri",
       "text": "Une école diplômante pour apprendre le français aux réfugiés (23/05/2016)"
     },
+    { "link": "https://www.mediapart.fr/journal/france/260516/migrants-comment-aider",
+      "title": "Mediapart",
+      "text": "Migrants: comment aider? (26/05/2016)"
+    },
     { "link": "http://www.franceculture.fr/emissions/rue-des-ecoles/rue-des-ecoles-dimanche-29-mai-2016",
       "title": "France Culture (\"Rue des écoles\")",
       "text": "Focus : \"Thot, une école pour les migrants\", avec Judith Aquien, présidente de Thot à 48' (29/05/2016)"
+    },
+    { "link": "http://www.lesinrocks.com/2016/05/31/actualite/12-femmes-bousculer-quotidien-11826880/",
+      "title": "Les Inrocks",
+      "text": "12 femmes qui vont bousculer votre quotidien (31/05/2016)"
     }
 ]
 
